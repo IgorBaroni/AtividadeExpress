107c8683af3cf9ddf6924c9651944fe3b75bca31
IGOR  ASSIS BARONI <0221432311022@fatectq.local>
2024-08-28 11:52:45 -0300

    fix: docs & users json

--- server.js
+++ server.js
@@ -1,86 +1,11 @@
-const { createServer } = require("node:http");
 const express = require('express');
 const { Router } = require('express');
 
+const users = require('./users.json')
+const docs = require('./docs.json')
+
 const app = express();
 const route = Router();
-
-const users = [
-  {
-    id: 1,
-    name: "Alice Silva",
-    email: "alice.silva@example.com",
-    senha: "alice234234",
-  },
-  {
-    id: 2,
-    name: "Bruno Costa",
-    email: "bruno.costa@example.com",
-    senha: "brunoCosta3453",
-  },
-  {
-    id: 3,
-    name: "Carla Pereira",
-    email: "carla.pereira@example.com",
-    senha: "c.arla435",
-  },
-  {
-    id: 4,
-    name: "David Lima",
-    email: "david.lima@example.com",
-    senha: "david3342",
-  },
-  {
-    id: 5,
-    name: "Elena Rocha",
-    email: "elena.rocha@example.com",
-    senha: "elena321rocha",
-  },
-];
-
-const docs = [
-  {
-    id: 1,
-    id_user: 1,
-    tipo: "Trabalho",
-    titulo: "atividade prática",
-    curso: "ADS",
-    turno: "manhã",
-  },
-  {
-    id: 2,
-    id_user: 1,
-    tipo: "Relatório",
-    titulo: "análise de dados",
-    curso: "Engenharia",
-    turno: "tarde",
-  },
-  {
-    id: 3,
-    id_user: 3,
-    tipo: "Pesquisa",
-    titulo: "estudo de mercado",
-    curso: "Administração",
-    turno: "noite",
-  },
-  {
-    id: 4,
-    id_user: 2,
-    tipo: "Apresentação",
-    titulo: "projeto final",
-    curso: "Design",
-    turno: "manhã",
-  },
-  {
-    id: 5,
-    id_user: 2,
-    tipo: "Ensaios",
-    titulo: "trabalho de literatura",
-    curso: "Jornalismo",
-    turno: "tarde",
-  },
-];
-
 
 app.use(express.json())
 
@@ -113,7 +38,6 @@ route.get('*', (req, res) => {
         message: `Erro ${res.statusCode}: Rota não encontrada!`,
       }));
 })
-
 
 const port = 3300;
 
